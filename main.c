a438261ab3f6b0f17840078d83bfda71c8734d49
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2021-04-28 17:36:52 +0200

    main

--- main.c
+++ main.c
@@ -1,10 +1,46 @@
 
 #include "list.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    return 0;
+int main() {
+    char ss;
+    int bb;
+    printf("m: %s", ss);
+    scanf("%s", ss);
+
+
+    switch (ss) {
+        case 'a':
+            bb= 2;
+            printf("%i",bb);
+            break;
+        case 'e':
+            bb= 5;
+            printf("%i", bb);
+            break;
+        case 'i':
+            bb= 6;
+            printf("%i", bb);
+            break;
+        case 'u':
+            bb= 1;
+            printf("%i", bb);
+
+            break;
+        case 'l':
+            bb= 9;
+            printf("%i", bb);
+
+            break;
+    }
+
+    return 1;
 }
+
+
+
+
 
 
 
